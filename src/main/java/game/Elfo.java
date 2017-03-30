bea7dc3d9c39b8788df5365a244b76f1eaa3b20e
maro01507i <ivan.martin@opendeusto.es>
2017-03-30 11:06:06 +0200

    Added more lines to Elfo.java

--- src/main/java/game/Elfo.java
+++ src/main/java/game/Elfo.java
@@ -3,5 +3,16 @@ package game;
 //Clase de prueba de un elfo
 
 public class Elfo extends Character{
+
+	public Elfo(WeaponBehaviour w) {
+		super(w);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void display() {
+		// TODO Auto-generated method stub
+		System.out.println("I´m an Elfo");
+	}
 	
 }
