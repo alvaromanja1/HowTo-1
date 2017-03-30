f269b7594be0d6d64a38e1da55fc8a71373c6f13
maro01507i <ivan.martin@opendeusto.es>
2017-03-30 10:50:14 +0200

    Added another test message for creating a conflict

--- src/main/java/game/Sword.java
+++ src/main/java/game/Sword.java
@@ -12,6 +12,7 @@ public class Sword implements WeaponBehaviour {
 		// TODO Auto-generated method stub
 		System.out.println("You slash madly with your sword.");
 		System.out.println("Test message.");
+		System.out.println("Added another test message v2");
 	}
 }
 
